fab864e991925e3537b374fd4c339f4f294d2846
sun9114a <sun9114a@gmail.com>
2026-05-14 15:19:41 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/heparin', (req, res) => {
+
+    const weight = Number(req.body.userRequest.utterance);
+
+    const contexts = req.body.contexts || [];
+
+    let protocol = '';
+
+    contexts.forEach(c => {
+
+        if (c.name === 'protocol') {
+
+            protocol = c.params.type.value;
+        }
+    });
+
+    let bolus = 0;
+    let infusion = 0;
+
+    if (protocol === 'MI') {
+
+        bolus = weight * 60;
+
+        if (bolus > 4000) {
+            bolus = 4000;
+        }
+
+        infusion = weight * 12;
+
+        if (infusion > 1000) {
+            infusion = 1000;
+        }
+
+    } else {
+
+        bolus = weight * 80;
+        infusion = weight * 18;
+    }
+
+    const mlhr = (infusion / 50).toFixed(1);
+
+    const result =
+`[${protocol} protocol]
+
+체중: ${weight} kg
+
+Bolus:
+${bolus} units IV
+
+Initial infusion:
+${infusion} units/hr
+
+(${mlhr} ml/hr)`;
+
+    res.json({
+        version: "2.0",
+        template: {
+            outputs: [
+                {
+                    simpleText: {
+                        text: result
+                    }
+                }
+            ]
+        }
+    });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('server start');
+});
